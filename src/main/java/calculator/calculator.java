8f7b7755481debbe6ec77213ba0f9417ec74def2
asllsa52 <asllsa52@github.com>
2025-10-20 23:53:43 +0900

    feat: refator:

--- src/main/java/calculator/calculator.java
+++ src/main/java/calculator/calculator.java
@@ -5,7 +5,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class calculator {
-	
+
     public static int add(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
@@ -14,23 +14,23 @@ public class calculator {
         String delimiter = ",|:";
 
         Matcher matcher = Pattern.compile("//(.*)\n(.*)").matcher(input);
-        
+
         if (matcher.matches()) {
             delimiter = Pattern.quote(matcher.group(1));
-            input = matcher.group(2); 
+            input = matcher.group(2);
         }
-        
+
         String allowedChars = "[0-9" + delimiter.replace("|", "") + "]+";
         String inputWithoutChars = input.replaceAll("\\s+", "");
-        
+
         if (!Pattern.matches("(" + allowedChars + ")+", inputWithoutChars.replaceAll(delimiter, ""))) {
             throw new IllegalArgumentException("허용되지 않은 구분자/문자 포함 : " + input);
         }
-        
+
         String[] tokens = input.split(delimiter);
 
         int sum = 0;
-        
+
         for (String token : tokens) {
             Matcher numMatcher = Pattern.compile("-?\\d+").matcher(token);
             while (numMatcher.find()) {
@@ -46,25 +46,16 @@ public class calculator {
     }
 
     public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-    	while (true) {
-    		System.out.print("문자열 입력 : ");
-    		String input = sc.nextLine();
-            if (input.equalsIgnoreCase("close")) break;
-            
-            try {
-                int result = add(input);
-                System.out.println("결과 : " + result);
-            } catch (IllegalArgumentException error) {
-                System.err.println("오류 발생 : " + error.getMessage());
-                System.out.println("프로그램 종료.");
-                System.exit(1);
-            } catch (Exception error) {
-                System.err.println("예상치 못한 오류 : " + error.getMessage());
-            }
-    	}
-    	sc.close();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = sc.nextLine();
+
+        int result = add(input);
+        System.out.println("결과 : " + result);
+
+        sc.close();
     }
-    
+
 }
 
